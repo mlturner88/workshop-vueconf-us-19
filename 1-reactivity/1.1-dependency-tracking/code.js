db5b60056d275b69ee93d541a42b08910b8d765b
Matthew Turner <mlturner88@outlook.com>
2019-03-25 10:30:01 -0400

    Completed 1.1

--- 1-reactivity/1.1-dependency-tracking/code.js
+++ 1-reactivity/1.1-dependency-tracking/code.js
@@ -1,7 +1,27 @@
-export class Observable {
+let currentRunner;
 
+export class Observable {
+  constructor(initialValue) {
+    this.value = initialValue;
+    this.subscribers = new Set();
+  }
+
+  get() {
+    if (currentRunner) {
+      this.subscribers.add(currentRunner);
+    }
+
+    return this.value;
+  }
+
+  set(v) {
+    this.value = v;
+    this.subscribers.forEach(sub => sub());
+  }
 }
 
 export function effect(runner) {
-
+  currentRunner = runner;
+  runner();
+  currentRunner = null;
 }
