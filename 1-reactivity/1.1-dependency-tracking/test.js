93fe218e6bdde7e4e5b4bdf4ac071d569325e14d
Matthew Turner <mlturner88@outlook.com>
2019-03-25 11:34:08 -0400

    Formatting

--- 1-reactivity/1.1-dependency-tracking/test.js
+++ 1-reactivity/1.1-dependency-tracking/test.js
@@ -1,14 +1,14 @@
-import { Observable, effect } from './code'
+import { Observable, effect } from "./code";
 
-it('should work', () => {
-  let result = null
-  const count = new Observable(1)
+it("should work", () => {
+  let result = null;
+  const count = new Observable(1);
 
   effect(() => {
-    result = count.get()
-  })
-  expect(result).toBe(1)
+    result = count.get();
+  });
+  expect(result).toBe(1);
 
-  count.set(2)
-  expect(result).toBe(2)
-})
+  count.set(2);
+  expect(result).toBe(2);
+});
