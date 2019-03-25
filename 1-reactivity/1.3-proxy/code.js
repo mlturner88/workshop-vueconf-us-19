184e896cd978d99a137efedea851e7301664e06e
Matthew Turner <mlturner88@outlook.com>
2019-03-25 11:33:54 -0400

    Completed 1.3

--- 1-reactivity/1.3-proxy/code.js
+++ 1-reactivity/1.3-proxy/code.js
@@ -1,8 +1,38 @@
-import { Dep, effect } from '../1.2-getter-setter/code.js'
+import { Dep, effect } from "../1.2-getter-setter/code.js";
 
-export { effect }
+export { effect };
 
-// ---
+const handlers = {
+  get(target, key) {
+    const dep = getDep(target, key);
+    dep.depend();
+    return target[key];
+  },
+
+  set(target, key, value) {
+    const dep = getDep(target, key);
+    target[key] = value;
+    dep.notify();
+    return true;
+  }
+};
+
 export function observable(obj) {
+  return new Proxy(obj, handlers);
+}
 
+const depsMap = new WeakMap();
+
+function getDep(target, key) {
+  if (!depsMap.has(target)) {
+    depsMap.set(target, {});
+  }
+
+  const deps = depsMap.get(target);
+
+  if (!deps[key]) {
+    deps[key] = new Dep();
+  }
+
+  return deps[key];
 }
